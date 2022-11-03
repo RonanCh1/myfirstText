5d36f757727e77e98e9267b2c0ef4c3dde234463
RonanC <116558301+RonanCh1@users.noreply.github.com>
2022-11-03 09:35:26 +0000

    YO 1.2

--- main.py
+++ main.py
@@ -1,26 +1,61 @@
 import random
+import sys
 
 
 class Character():
-    def __init__(self):
-        self.name = ""
-        self.hp = 100
-        self.ap = 110
+    def __init__(self, name, health = "100", attack = "20", mana= "100", stamina = "50"):
+        self.name = name
+        self.hp = health
+        self.ap = attack
+        self.mp = mana
+        self.stam = stamina
 
     def take_Dmg(self, dmg):
         self.hp -= dmg
+        if self.hp < 1:
+            self.hp = 0
         return self.hp
+
+    def get_atk(self,defense_roll):
+        atk = random.randint(0, self.ap)
+              
+        if atk < defense_roll:
+          atk = 0
+        else:
+          atk -= defense_roll
+        return atk
 
     def is_dead(self):
         if self.hp < 1:
-            self.hp = 0
-            return True,
+            return True
         else:
-            False
+            return False
 
 
-pc = Character()
-dmg_amt = random.randint(0, pc.ap)
-print("the pc took " + str(dmg_amt) + " Damage")
-#print("the pc has",  ,"health")
-print(f"the pc has {pc.take_Dmg(dmg_amt)} health ")
+class End_state():
+  pass
+class Story():
+    def __init__(self):
+        print("Welcome Traveler")
+        self.new_game()
+    
+    def new_game(self):
+      self.create_character()
+
+    def create_character(self):
+      pc_name = input("Enter character name \n -> ")
+      pc = Character(pc_name)
+
+    def outside(self):
+      print("You stand here  ")
+      print("cool sesond line that inspires ")
+      print("really cool third line ")
+
+      pc_input = input("Enter?    \n ->")
+      if pc_input == "yes":
+        print("yayayayaya")
+        pass #choices for player 
+      else:
+        sys.exit() #end game 
+newgame = Story()
+
