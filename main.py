dfda142cba8cf424e80c8614f6784f794ab3e0c4
RonanC <116558301+RonanCh1@users.noreply.github.com>
2022-11-07 12:00:25 +0000

    no

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import sys
 
 
 class Character():
-    def __init__(self, name, health = "100", attack = "20", mana= "100", stamina = "50"):
+    def __init__(self,
+                 name,
+                 health="100",
+                 attack="20",
+                 mana="100",
+                 stamina="50"):
         self.name = name
         self.hp = health
         self.ap = attack
@@ -16,13 +21,13 @@ class Character():
             self.hp = 0
         return self.hp
 
-    def get_atk(self,defense_roll):
+    def get_atk(self, defense_roll):
         atk = random.randint(0, self.ap)
-              
+
         if atk < defense_roll:
-          atk = 0
+            atk = 0
         else:
-          atk -= defense_roll
+            atk -= defense_roll
         return atk
 
     def is_dead(self):
@@ -33,29 +38,42 @@ class Character():
 
 
 class End_state():
-  pass
+      def __init__(self):
+        print("ending")
+        sys.exit()
+
 class Story():
     def __init__(self):
         print("Welcome Traveler")
         self.new_game()
-    
+
+    def validate(self, question, answers):
+      answer = " "
+      while answer not in answers:
+        print("please prov answer")
+        answer  = input(question)
+
+      return answer
     def new_game(self):
-      self.create_character()
+        self.create_character()
 
     def create_character(self):
-      pc_name = input("Enter character name \n -> ")
-      pc = Character(pc_name)
+        pc_name = input("Enter character name \n -> ")
+        pc = Character(pc_name)
+        self.outside(pc)
 
-    def outside(self):
-      print("You stand here  ")
-      print("cool sesond line that inspires ")
-      print("really cool third line ")
+    def outside(self, pc):
+        
+        print("You stand here  ")
+        print("cool sesond line that inspires ")
+        print("really cool third line ")
 
-      pc_input = input("Enter?    \n ->")
-      if pc_input == "yes":
-        print("yayayayaya")
-        pass #choices for player 
-      else:
-        sys.exit() #end game 
+        pc_input = self.validate("Enter?    \n ->",["yes","no"])
+        if pc_input == "yes":
+            print(f"yayayayaya {pc.name}")
+            pass  #choices for player
+        else:
+            print("wawawa")
+
+
 newgame = Story()
-
